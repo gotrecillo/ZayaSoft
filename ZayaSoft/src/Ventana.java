9fc802aab958f1dd7d459ec9546d8b08d6ff76dd
gotrecillo <sergio.panadero.perez@gmail.com>
2015-04-27 12:42:55 +0200

    main window

---
+++ ZayaSoft/src/Ventana.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+
+public class Ventana {
+	private JFrame mainFrame;
+
+	public Ventana() {
+		mainFrame = new JFrame("Calculadora");
+		mainFrame.setSize(600, 600);
+		mainFrame.setVisible(true);
+	}
+	
+}
